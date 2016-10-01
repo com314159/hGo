22f2c521cad48c2be23c034633d3948ea7ab0a52
LiuYihan <641872038@qq.com>
2016-10-01 13:31:33 +0800

    add ex1

---
+++ ex1.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+  var user_name string = "liuyihan"
+  gender := "male"
+
+  fmt.Println("What a great boy")
+  fmt.Println(user_name + "'s gender is " + gender)
+}
